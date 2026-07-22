bef564a219f0bd721af631c30700d902a583e495
krishnakarthik51431-netizen <krishnakarthik51431@gmail.com>
2026-07-22 12:44:07 +0530

    var in JS is not block scoped

--- Day/Day8/P4.js
+++ Day/Day8/P4.js
@@ -1,5 +1,11 @@
-// variable in JS
+// Variables in JS
 var x = 10;
 var x = 20;
 x = 30;
 console.log(x)
+
+if (true) {
+    var message = "Hello";
+}
+// var in JS is not block-scoped
+console.log(message);
