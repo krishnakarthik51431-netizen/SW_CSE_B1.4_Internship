c8dbddd33f3d23be4ec20e3bfde336acf404f896
krishnakarthik51431-netizen <krishnakarthik51431@gmail.com>
2026-07-29 12:56:08 +0530

    Props example 2

--- Day/Day13/day/src/App.jsx
+++ Day/Day13/day/src/App.jsx
@@ -1,6 +1,46 @@
-// Named export
+import Button from './components/FunctionalComponent'
+import { LeftArrow,RightArrow } from "./components/FunctionalComponentTwo";
 
-export const LeftArrow = () => <span>⬅️</span> 
-export const RightArrow = () => <span>➡️</span>
+import ProfileList from "./components/Props";
+import InfoBoxDemo from "./components/PropsTwo";
 
-// windows + > : emojis
+import './App.css'
+
+const name = "Amay";
+const isLoggedIn = false;
+
+function App() {
+ 
+  return (
+    // React Fragments
+    <>
+      {/* JSX Rule 1: Return one parent element */}
+      <h1>Hello</h1>
+      <p>Hello again</p>
+
+      {/* JSX Rule 2: Every tag must be closed properly */}
+      <p>Error</p>
+      <input type="text" />
+
+      {/* JSX Rule 3: use camelCase for attributes */}
+      <button onClick onSubmit>Click</button>
+
+      {/* JSX Rule 4: Embedding JavaScript with curly braces */}
+      <h3>Hello, {name}</h3>
+
+      {/* JSX Rule 5: use expressions, not statements */}
+      <p>{isLoggedIn ? "Welcome back" : "Please Login"}</p>
+
+      <Button />
+      <LeftArrow />
+      <RightArrow />
+
+      <ProfileList />
+
+      <InfoBoxDemo />
+    </>
+    
+  )
+}
+
+export default App
