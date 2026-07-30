d5b818798ad61501f4932fd3a1f68be722e7b153
krishnakarthik51431-netizen <krishnakarthik51431@gmail.com>
2026-07-30 15:28:41 +0530

    Form example 1

--- Day/Day14/day14-react-app/src/App.jsx
+++ Day/Day14/day14-react-app/src/App.jsx
@@ -5,6 +5,8 @@ import ProductListDemo from "./components/ListThree";
 import CounterHook from "./components/HooksOne";
 import UserListHook from "./components/HooksTwo";
 import ThemeProviderDemo from "./components/HooksThree";
+import SimpleNameForm from "./components/FormOne";
+
 function App() {
   
   return (
@@ -15,7 +17,8 @@ function App() {
 
       <CounterHook/> */}
       {/* <UserListHook/> */}
-      <ThemeProviderDemo />
+      {/* <ThemeProviderDemo /> */}
+      <SimpleNameForm />
     </>
   )
 }
