1063eef6d76ee41aaf54f9dd64c432789d7decf4
krishnakarthik51431-netizen <krishnakarthik51431@gmail.com>
2026-07-30 15:07:23 +0530

    Hooks: usecontext

--- Day/Day14/day14-react-app/src/App.jsx
+++ Day/Day14/day14-react-app/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import SimpleList from "./components/ListOne";
 import UserList from "./components/ListTwo";
 import ProductListDemo from "./components/ListThree";
-import CounterHook from "./components/HooksOne";
-import UserListHook from "./components/HooksTwo";
-
+import CounterHook from "./components/HookOne";
+import UserListHook from "./components/HookTwo";
+import ThemeProviderDemo from "./components/HookThree";
 function App() {
   
   return (
@@ -14,7 +14,8 @@ function App() {
       <ProductListDemo />
 
       <CounterHook/> */}
-      <UserListHook/>
+      {/* <UserListHook/> */}
+      <ThemeProviderDemo />
     </>
   )
 }
